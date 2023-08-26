9e3f2213c6472cce90067931081d68d92cd1c883
Oleg Sinelnikov <oleg.texet@gmail.com>
2023-08-26 08:04:05 +0300

    Init

---
+++ app/src/main/java/ru/olegsvs/wolsettings/MainActivity.kt
@@ -0,0 +1,88 @@
+package ru.olegsvs.wolsettings
+
+import android.os.Bundle
+import android.os.StrictMode
+import android.preference.PreferenceManager
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.*
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.tooling.preview.Preview
+import ru.olegsvs.wolsettings.ui.theme.WolSettingsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val policy = StrictMode.ThreadPolicy.Builder().permitAll().build()
+        StrictMode.setThreadPolicy(policy)
+        setContent {
+            WolSettingsTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colors.background) {
+                    screen()
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun screen() {
+    Column() {
+        val prefs = PreferenceManager.getDefaultSharedPreferences(LocalContext.current)
+        var dstAddress by rememberSaveable { mutableStateOf(prefs.getString("DST_MAC", "2C:F0:5D:87:9B:A1")) }
+        var ip by rememberSaveable { mutableStateOf(prefs.getString("GATEWAY_IP", "192.168.1.255")) }
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = dstAddress!!,
+            onValueChange = {
+                dstAddress = it
+            },
+            label = { Text("DST MAC ADDRESS") }
+        )
+        TextField(
+            modifier = Modifier.fillMaxWidth(),
+            value = ip!!,
+            onValueChange = {
+                ip = it
+            },
+            label = { Text("LAN GATEWAY IP") }
+        )
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                prefs.edit().putString("DST_MAC", dstAddress).putString("GATEWAY_IP", ip).apply()
+            }) {
+            Text("SAVE")
+        }
+        Button(
+            modifier = Modifier.fillMaxWidth(),
+            onClick = {
+                val prefsDST = prefs.getString("DST_MAC", "")
+                val prefsIP = prefs.getString("GATEWAY_IP", "")
+                if (prefsIP != null && prefsIP.isNotEmpty() && prefsDST != null && prefsDST.isNotEmpty()) {
+                    WolUtils.wakeUp(prefsIP, prefsDST)
+                }
+            }) {
+            Text("TEST")
+        }
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    WolSettingsTheme(darkTheme = true) {
+        screen()
+    }
+}
---
+++ app/src/main/java/ru/olegsvs/wolsettings/WolUtils.kt
@@ -0,0 +1,51 @@
+package ru.olegsvs.wolsettings
+
+import android.util.Log
+import java.net.DatagramPacket
+import java.net.DatagramSocket
+import java.net.InetAddress
+
+class WolUtils {
+    companion object {
+        fun wakeUp(
+            ip: String,
+            mac: String
+        ) {
+            DatagramSocket().use { socket ->
+                try {
+                    val macBytes = getMacBytes(mac)
+                    val bytes = ByteArray(6 + 16 * macBytes.size)
+                    for (i in 0..5) {
+                        bytes[i] = 0xff.toByte()
+                    }
+                    var i = 6
+                    while (i < bytes.size) {
+                        System.arraycopy(macBytes, 0, bytes, i, macBytes.size)
+                        i += macBytes.size
+                    }
+                    val address: InetAddress = InetAddress.getByName(ip)
+                    val packet = DatagramPacket(bytes, bytes.size, address, 9)
+                    socket.send(packet)
+                    Log.i("WoL", "Wake-on-LAN packet sent.")
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+
+        @Throws(IllegalArgumentException::class)
+        private fun getMacBytes(macStr: String): ByteArray {
+            val bytes = ByteArray(6)
+            val hex = macStr.split(":").toTypedArray()
+            require(hex.size == 6) { "Invalid MAC address." }
+            try {
+                for (i in 0..5) {
+                    bytes[i] = hex[i].toInt(16).toByte()
+                }
+            } catch (e: NumberFormatException) {
+                throw IllegalArgumentException("Invalid hex digit in MAC address. ${e.toString()}")
+            }
+            return bytes
+        }
+    }
+}
